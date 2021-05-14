df48f95fdfb0a468a830449d586817bf2d262d11
Shawn Contant <shawnc366@gmail.com>
2021-05-13 19:47:28 -0500

    The file for making server calls

---
+++ src/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { api_url } from './config';
+
+class Api {
+    static get(route, data) {
+        return axios.get(api_url + route, data);
+    }
+
+    static post(route, data) {
+        return axios.post(api_url + route, data);
+    }
+
+    static put(route, data) {
+        return axios.put(api_url + route, data);
+    }
+
+    static del(route, data) {
+        return axios.delete(api_url + route, data);
+    }
+}
